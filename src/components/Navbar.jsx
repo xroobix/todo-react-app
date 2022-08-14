78f49d7f6a164861fb3f31a2b24e02110fca41f3
xroobix <109462535+xroobix@users.noreply.github.com>
2022-08-14 17:13:40 +0200

    FEAT: Added animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,20 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 export default function Navbar() {
+  console.log('render navbar');
+
   return (
     <nav className="navbar">
-      <h1>Todo App</h1>
-      <div className="navbar list">
-        <Link to="/">Inbox</Link>
-        <Link to="/today">Today</Link>
-        <Link to="/nextweek">Next 7 days</Link>
+      <h1 className="navbar__title">Todo App</h1>
+      <div className="navbar__list">
+        <NavLink to="" className="link">
+          <span>Inbox</span>
+        </NavLink>
+        <NavLink to="today" className="link">
+          <span>Today</span>
+        </NavLink>
+        <NavLink to="nextweek" className="link">
+          <span>Next 7 days</span>
+        </NavLink>
       </div>
     </nav>
   );
