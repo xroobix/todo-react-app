7400dd97656832d62b77e635ed3ef4610e5e893f
xroobix <109462535+xroobix@users.noreply.github.com>
2022-08-25 19:08:46 +0200

    STYLE: formated with prettier

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-export default function NotFound(){
-    return(
-        <div className="notfound">
-            <p>¯\_(ツ)_/¯</p>
-            <h1>Oops...</h1>
-            <h5>We can't find the task you're looking for.</h5>
-            <Link to="" className="go-back">Click here to go back</Link>
-        </div>
-    )
+export default function NotFound() {
+  return (
+    <div className="notfound">
+      <p>¯\_(ツ)_/¯</p>
+      <h1>Oops...</h1>
+      <h5>We can't find the task you're looking for.</h5>
+      <Link to="" className="go-back">
+        Click here to go back
+      </Link>
+    </div>
+  );
 }
