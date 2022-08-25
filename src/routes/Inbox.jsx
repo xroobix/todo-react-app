0f9b21c36aca83d863f94901c4aa5f201f81fb2d
xroobix <109462535+xroobix@users.noreply.github.com>
2022-08-25 19:05:47 +0200

    REFACTOR: replaced useFetch hook

--- src/routes/Inbox.jsx
+++ src/routes/Inbox.jsx
@@ -1,19 +1,46 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import TodoList from '../components/TodoList';
-import useFetch from '../hooks/useFetch';
 export default function Inbox() {
-  const {
-    data: tasks,
-    isLoading,
-    error,
-  } = useFetch('http://127.0.0.1:8000/tasks');
+  const [shouldFetch, setShouldFetch] = useState(false);
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/tasks')
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Couldn't fetch data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+        setError(null);
+        setShouldFetch(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err.message);
+      });
+  }, [shouldFetch]);
+
+  function handleChange() {
+    setShouldFetch(true);
+    console.log("refetched")
+    console.log(shouldFetch)
+  }
 
   return (
     <>
-      {error && <h2 className='error'>{error}</h2>}
-      {isLoading && <h2 className='loading'>Loading...</h2>}
-      {tasks ? (
-        <TodoList tasks={tasks} />
+      {isLoading ? (
+        <h2 className="loading">Loading...</h2>
+      ) : error ? (
+        <h2 className="error">{error}</h2>
+      ) : data ? (
+        <TodoList tasks={data} handleChange={handleChange} />
       ) : (
         <Link to="/add" className="btn">
           Create Todo
