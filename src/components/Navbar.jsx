67b22085bb14bd7d4025c3e8c7483643ff83cbcd
xroobix <109462535+xroobix@users.noreply.github.com>
2022-09-05 13:28:23 +0200

    Removed links from desktop navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,16 @@
 import { AiFillCarryOut } from 'react-icons/ai';
-import { NavLink } from 'react-router-dom';
+
 export default function Navbar() {
   const logoIcon = (
-    <AiFillCarryOut className="icon" size="40px" fill="#3ABFF8" />
+    <AiFillCarryOut className="icon" size="2.5em" fill="#3ABFF8" />
   );
 
   return (
-    <nav className="navbar">
-      <div className="list">
-        <NavLink to="" className="link">
-          Inbox
-        </NavLink>
-        <NavLink to="today" className="link">
-          Today
-        </NavLink>
-        <NavLink to="nextweek" className="link">
-          Next 7 days
-        </NavLink>
-        <NavLink to="add" className="link">
-          Add Todo
-        </NavLink>
-      </div>
+    <div className="navbar">
       <div className="logo">
-        <h1 className="title">Todos App</h1>
+        <h1 className="title">ToDo App</h1>
         {logoIcon}
       </div>
-    </nav>
+    </div>
   );
 }
