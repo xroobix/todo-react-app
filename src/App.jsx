780f50f3838982d6025d139b0eff8c51740dbc67
xroobix <109462535+xroobix@users.noreply.github.com>
2022-09-05 13:07:10 +0200

    FEAT: added width listener

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import NavbarMobile from './components/NavbarMobile';
+import Sidebar from './components/Sidebar';
 import Add from './routes/Add';
 import Edit from './routes/Edit';
 import Inbox from './routes/Inbox';
@@ -9,10 +12,26 @@ import NotFound from './routes/NotFound';
 import Today from './routes/Today';
 
 function App() {
+  const width = useWindowSize();
+
+  function useWindowSize() {
+    const [size, setSize] = useState(0);
+    useEffect(() => {
+      function updateSize() {
+        setSize(window.innerWidth);
+      }
+      window.addEventListener('resize', updateSize);
+      updateSize();
+      return () => window.removeEventListener('resize', updateSize);
+    }, []);
+    return size;
+  }
+
   return (
     <>
-      <Navbar />
+      {width <= 768 ? <NavbarMobile /> : <Navbar />}
       <main className="content">
+        {width > 768 && <Sidebar />}
         <Routes>
           <Route path="/" element={<Inbox />} />
           <Route path="/today" element={<Today />} />
